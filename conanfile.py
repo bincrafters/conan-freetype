b8e0cb93a761437c8f2491a4e96f55985a72a845
Croydon <cr0ydon@outlook.com>
2019-03-29 13:37:42 +0100

    Correct license: Freetype uses the FTL license
    https://www.freetype.org/license.html

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class FreetypeConan(ConanFile):
     description = "FreeType is a freely available software library to render fonts."
     url = "http://github.com/bincrafters/conan-freetype"
     homepage = "https://www.freetype.org"
-    license = "BSD"
+    license = "FTL"
     topics = ("conan", "freetype", "fonts")
     author = "Bincrafters <bincrafters@gmail.com>"
     exports = ["LICENSE.md"]
