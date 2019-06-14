3ce227724130c5ec263c98ab43daf1ac1e2688a8
SSE4 <tomskside@gmail.com>
2019-06-14 23:21:32 +0200

    - bump libpng to 1.6.37

--- conanfile.py
+++ conanfile.py
@@ -38,7 +38,7 @@ class FreetypeConan(ConanFile):
 
     def requirements(self):
         if self.options.with_png:
-            self.requires.add("libpng/1.6.36@bincrafters/stable")
+            self.requires.add("libpng/1.6.37@bincrafters/stable")
         if self.options.with_zlib:
             self.requires.add("zlib/1.2.11@conan/stable")
         if self.options.with_bzip2:
